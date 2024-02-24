8c158d1afbf0c61dc8ae8e584414ede05c64fdb8
sushma@-1234 <sushmareddymaruri@gmail.com>
2024-02-24 13:59:04 +0530

    task

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
-
+export class RegisterComponent implements OnInit {
+  constructor(
+    private location : Location
+  ){}
+  ngOnInit(): void {
+    
+  }
+  goBack(){
+    this.location.back();
+  }
 }
+
+
+
